3fa6eb486bd908025e1da6753da9ca67ad41566b
leminhhieu98py <leminhhieu98py@gmail.com>
2024-01-19 14:46:57 +0700

    add server action

---
+++ app/lib/action.ts
@@ -0,0 +1,68 @@
+'use server';
+
+import { getRawFormData } from './utils';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const EditInvoice = FormSchema.omit({ date: true });
+
+export async function createInvoice(formData: FormData) {
+  try {
+    const rawFormData = getRawFormData(formData);
+    const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  } catch (error: any) {
+    throw Error(error.message);
+  }
+}
+
+export async function updateInvoice(formData: FormData) {
+  try {
+    const rawFormData = getRawFormData(formData);
+    const { customerId, amount, status, id } = EditInvoice.parse(rawFormData);
+
+    const amountInCents = amount * 100;
+
+    await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+          `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  } catch (error: any) {
+    throw Error(error.message);
+  }
+}
+
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  } catch (error: any) {
+    throw Error(error.message);
+  }
+}
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -92,3 +92,6 @@ export const getPathWithQueryParams = ({
 
   return { path: newPath };
 };
+
+export const getRawFormData = (formData: FormData) =>
+  Object.fromEntries(formData.entries());
